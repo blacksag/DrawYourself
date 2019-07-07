7eb32d59f3f9dcffa29415b19a3e919f30b6fafc
Blacksag <Blacksag@DESKTOP-7L6VC05>
2019-07-07 13:15:31 +0530

    Add project files.

---
+++ DrawYourself/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace DrawYourself
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tools = new System.Windows.Forms.Panel();
+            this.canvas = new System.Windows.Forms.Panel();
+            this.frame = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // tools
+            // 
+            this.tools.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.tools.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tools.Location = new System.Drawing.Point(0, 0);
+            this.tools.Name = "tools";
+            this.tools.Size = new System.Drawing.Size(798, 85);
+            this.tools.TabIndex = 0;
+            // 
+            // canvas
+            // 
+            this.canvas.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.canvas.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.canvas.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.canvas.Location = new System.Drawing.Point(74, 161);
+            this.canvas.MaximumSize = new System.Drawing.Size(256, 256);
+            this.canvas.MinimumSize = new System.Drawing.Size(256, 256);
+            this.canvas.Name = "canvas";
+            this.canvas.Size = new System.Drawing.Size(256, 256);
+            this.canvas.TabIndex = 1;
+            this.canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseDown);
+            this.canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseMove);
+            this.canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.canvas_MouseUp);
+            // 
+            // frame
+            // 
+            this.frame.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.frame.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.frame.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.frame.Location = new System.Drawing.Point(470, 161);
+            this.frame.MaximumSize = new System.Drawing.Size(256, 256);
+            this.frame.MinimumSize = new System.Drawing.Size(256, 256);
+            this.frame.Name = "frame";
+            this.frame.Size = new System.Drawing.Size(256, 256);
+            this.frame.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.button1.Location = new System.Drawing.Point(353, 264);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(93, 45);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(798, 504);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.frame);
+            this.Controls.Add(this.canvas);
+            this.Controls.Add(this.tools);
+            this.MinimumSize = new System.Drawing.Size(700, 560);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Draw Doodles";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel tools;
+        private System.Windows.Forms.Panel canvas;
+        private System.Windows.Forms.Panel frame;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DrawYourself/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace DrawYourself
+{
+    public partial class Form1 : Form
+    {
+        Pen myPen = new Pen(Color.Black, 1);
+        Graphics g = null;
+        bool draw = false;
+        Point start = new Point(0, 0);
+        Point end = new Point(0, 0);
+        List<List<Point>> pathList = new List<List<Point>>();
+        int strokes = 0;
+        //Point[] path;
+        public Form1()
+        {
+            InitializeComponent();
+            g = canvas.CreateGraphics();
+        }
+
+        private void canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                draw = true;
+                start = e.Location;
+                pathList.Add(new List<Point> { start });
+                g.FillRectangle(Brushes.Black, start.X, start.Y, 1, 1);
+                strokes++;
+            }
+            
+        }
+
+        private void canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (draw == true)
+            {
+                // get positions and draw on canvas
+                end = e.Location;
+                g.DrawLine(myPen, start, end);
+                start = end;
+                // also update the array according to the no of strokes
+                pathList[strokes-1].Add(end);
+            }
+        }
+
+        private void canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            draw = false;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            canvas.Refresh();
+            frame.Refresh();
+            Graphics gnew = frame.CreateGraphics();
+            //int length,width;
+            //int indx;
+            for (int i=0; i<strokes; i++)
+            {
+                if (pathList[i].Count == 1)
+                {
+                    gnew.FillRectangle(Brushes.Black, pathList[i][0].X, pathList[i][0].Y, 1, 1);
+                }
+                for (int j=1; j<pathList[i].Count; j++ )
+                {
+                    gnew.DrawLine(myPen, pathList[i][j - 1],pathList[i][j]);
+                    Thread.Sleep(10);
+                }
+                Thread.Sleep(50);
+
+                // divide the stroke in to small paths to animate it
+                /*length = pathList[i].Count;
+                width = (int)Math.Log(length/2 , 1.5);
+                indx = 0;
+                while((length-indx) > width)
+                {
+                    path = pathList[i].GetRange(indx,width).ToArray();
+                    gnew.DrawLines(myPen, path);
+                    indx += width;
+                    Thread.Sleep(120);
+                }
+                path = pathList[i].GetRange(indx, length - indx).ToArray();
+                if (path.Length > 1)
+                {
+                    gnew.DrawLines(myPen, path);
+                }
+                Thread.Sleep(120);*/
+
+                //simply show the drawing
+                /*path = pathList[i].ToArray();
+                gnew.DrawLines(myPen, path);*/
+            }
+            pathList.Clear();
+            strokes = 0;
+        }
+    }
+}
